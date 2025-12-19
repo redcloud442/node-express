048e6d64ffffa24e6e9c5797831ce3b5259e406f
redcloud442 <cloudred442@gmail.com>
2025-12-20 02:34:28 +0800

    add prisma

--- src/api/api.ts
+++ src/api/api.ts
@@ -1,4 +1,6 @@
+import { fromNodeHeaders } from "better-auth/node";
 import express, { type Router } from "express";
+import { auth } from "@/common/auth/auth";
 import { healthCheckRouter } from "./healthCheck/healthCheckRouter";
 import { userRouter } from "./user/userRouter";
 
@@ -7,3 +9,10 @@ export const api: Router = express.Router();
 api.use("/health-check", healthCheckRouter);
 
 api.use("/users", userRouter);
+
+api.get("/me", async (req, res) => {
+	const session = await auth.api.getSession({
+		headers: fromNodeHeaders(req.headers),
+	});
+	return res.json(session);
+});
--- src/server.ts
+++ src/server.ts
@@ -1,9 +1,11 @@
+import { toNodeHandler } from "better-auth/node";
 import cors from "cors";
 import express, { type Express } from "express";
 import helmet from "helmet";
 import { pino } from "pino";
 import { api } from "@/api/api";
 import { openAPIRouter } from "@/api-docs/openAPIRouter";
+import { auth } from "@/common/auth/auth";
 import errorHandler from "@/common/middleware/errorHandler";
 import rateLimiter from "@/common/middleware/rateLimiter";
 import requestLogger from "@/common/middleware/requestLogger";
@@ -18,7 +20,7 @@ app.set("trust proxy", true);
 // Middlewares
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use(cors({ origin: env.CORS_ORIGIN, credentials: true }));
+app.use(cors({ origin: env.CORS_ORIGIN, credentials: true, methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"] }));
 app.use(helmet());
 app.use(rateLimiter);
 
@@ -27,7 +29,7 @@ app.use(requestLogger);
 
 // Routes
 app.use("/api", api);
-
+app.all("/api/auth/*splat", toNodeHandler(auth));
 // Swagger UI
 app.use(openAPIRouter);
 
